3210395a2e59e8e049a50f4f8fe52a973ff543ca
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-05-21 21:42:42 +0530

    Tweakings

--- src/commands/scaffold_project.js
+++ src/commands/scaffold_project.js
@@ -9,40 +9,11 @@ const open = require('open');
 const mkdirp = require('mkdirp');
 const { execSync } = require('child_process');
 
-// Initial file content to be written to package.json
+// Initial file content to be written to package.json.
 const fileContent = ['{', '"name": "",', '"dependencies": {', '}', '}'];
 
+// Setting path to the template files.
 const templatePath = `${__dirname}/../templates`;
-
-// Starter template files
-const bootstrapThreeTemplate = fs.readFileSync(
-  `${templatePath}/bootstrap-3/index.html`,
-  'utf-8',
-);
-const bootstrapFourTemplate = fs.readFileSync(
-  `${templatePath}/bootstrap-4/index.html`,
-  'utf-8',
-);
-const materializeTemplate = fs.readFileSync(
-  `${templatePath}/materialize/index.html`,
-  'utf-8',
-);
-const foundationTemplate = fs.readFileSync(
-  `${templatePath}/foundation/index.html`,
-  'utf-8',
-);
-const semanticUiTemplate = fs.readFileSync(
-  `${templatePath}/semantic-ui/index.html`,
-  'utf-8',
-);
-const bulmaTemplate = fs.readFileSync(
-  `${templatePath}/bulma/index.html`,
-  'utf-8',
-);
-const basicTemplate = fs.readFileSync(
-  `${templatePath}/none/index.html`,
-  'utf-8',
-);
 
 const serveTemplate = projectName => {
   open(`${projectName}/index.html`);
@@ -90,50 +61,38 @@ exports.scaffoldProject = async projectName => {
   fileContent[1] = `"name": "${projectName}",`;
   fs.writeFileSync(`${projectName}/package.json`, fileContent.join('\n'));
 
-  inquirer
-    .prompt([
-      {
-        name: 'framework',
-        type: 'list',
-        message: 'Please choose the CSS framework of your choice\n',
-        choices: [
-          'Bootstrap-3',
-          'Bootstrap-4',
-          'Materialize',
-          'Foundation',
-          'Semantic-UI',
-          'Bulma',
-          'None',
-        ],
-      },
-    ])
-    .then(choice => {
-      mkdirp.sync(`${projectName}/static`);
-      mkdirp.sync(`${projectName}/static/stylesheets`);
-      fs.writeFileSync(
-        `${projectName}/static/stylesheets/style.css`,
-        '// Write your rulesets here!',
-      );
-      fs.writeFileSync(
-        `${projectName}/static/main.js`,
-        '// Any sorts of JavaScript code goes here!',
-      );
+  const { frameworkOfChoice } = await inquirer.prompt([
+    {
+      name: 'frameworkOfChoice',
+      type: 'list',
+      message: 'Please choose the CSS framework of your choice',
+      choices: [
+        'Bootstrap-3',
+        'Bootstrap-4',
+        'Materialize',
+        'Foundation',
+        'Semantic-UI',
+        'Bulma',
+        'None',
+      ],
+    },
+  ]);
+  mkdirp.sync(`${projectName}/static`);
+  mkdirp.sync(`${projectName}/static/stylesheets`);
 
-      if (choice.framework === 'Bootstrap-3') {
-        fs.writeFileSync(`${projectName}/index.html`, bootstrapThreeTemplate);
-      } else if (choice.framework === 'Bootstrap-4') {
-        fs.writeFileSync(`${projectName}/index.html`, bootstrapFourTemplate);
-      } else if (choice.framework === 'Materialize') {
-        fs.writeFileSync(`${projectName}/index.html`, materializeTemplate);
-      } else if (choice.framework === 'Foundation') {
-        fs.writeFileSync(`${projectName}/index.html`, foundationTemplate);
-      } else if (choice.framework === 'Semantic-UI') {
-        fs.writeFileSync(`${projectName}/index.html`, semanticUiTemplate);
-      } else if (choice.framework === 'Bulma') {
-        fs.writeFileSync(`${projectName}/index.html`, bulmaTemplate);
-      } else {
-        fs.writeFileSync(`${projectName}/index.html`, basicTemplate);
-      }
-      serveTemplate(projectName);
-    });
+  fs.writeFileSync(
+    `${projectName}/static/stylesheets/style.css`,
+    '// Write your rulesets here!',
+  );
+  fs.writeFileSync(
+    `${projectName}/static/main.js`,
+    '// Any sorts of JavaScript code goes here!',
+  );
+
+  const template = fs.readFileSync(
+    `${templatePath}/${frameworkOfChoice.toLowerCase()}/index.html`,
+    'utf-8',
+  );
+  fs.writeFileSync(`${projectName}/index.html`, template);
+  serveTemplate(projectName);
 };
