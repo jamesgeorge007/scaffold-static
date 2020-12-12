aa2517ac6d50bf9397af7912699cbffd2b6c74bb
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-12-12 23:44:46 +0530

    chore: configure jest for e2e tests

---
+++ __e2e__/commands/new.test.ts
@@ -0,0 +1,64 @@
+import fs from 'fs';
+import path from 'path';
+
+import run from '../test-utils';
+
+jest.setTimeout(300000);
+
+const testDirPath = path.join(__dirname, 'test-app');
+
+beforeAll(() => {
+  if (fs.existsSync(testDirPath)) {
+    fs.rmdirSync(testDirPath, { recursive: true });
+  }
+});
+
+afterAll(() => {
+  fs.rmdirSync(testDirPath, { recursive: true });
+});
+
+const generatedFiles = [
+  'css/style.css',
+  'js/main.js',
+  'index.html',
+  'webpack.config.js',
+  'package.json',
+  'yarn.lock',
+];
+
+const deps = [
+  'webpack',
+  'webpack-cli',
+  'webpack-dev-server',
+  'css-loader',
+  'style-loader',
+  'html-webpack-plugin',
+];
+
+describe('new command', () => {
+  it('creates a new project', async () => {
+    const { exitCode } = await run(['new', 'test-app'], {
+      cwd: __dirname,
+      input: '\n',
+    });
+
+    // Assertions
+    expect(exitCode).toBe(0);
+    generatedFiles.forEach(file =>
+      expect(fs.existsSync(path.join(testDirPath, file))).toBeTruthy()
+    );
+
+    const pkgJson = JSON.parse(
+      fs.readFileSync(path.join(testDirPath, 'package.json'))
+    );
+
+    // Assertion for the installed dependencies
+    expect(pkgJson.dependencies).toBeFalsy();
+    deps.forEach(dep => expect(pkgJson.devDependencies[dep]).toBeTruthy());
+
+    // Assertion for the configured scripts
+    expect(pkgJson.name).toBe('test-app');
+    expect(pkgJson.scripts['build']).toBe('webpack');
+    expect(pkgJson.scripts['serve']).toBe('webpack-dev-server --open');
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const common = { testEnvironment: 'node' };
+
+module.exports = {
+  projects: [
+    {
+      ...common,
+      displayName: 'e2e',
+      testMatch: ['<rootDir>/__e2e__/**/*.test.[jt]s'],
+    },
+  ],
+};
